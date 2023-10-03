028943f8e5fd38bb1e5c8ea91e52e1b549c6bc90
cadeelliott <137833718+cadeelliott@users.noreply.github.com>
2023-10-03 16:55:20 -0500

    Update popup.js
    added url value

--- popup.js
+++ popup.js
@@ -2,6 +2,6 @@ document.getElementById("copy-button").addEventListener("click", function() {
   const url = document.getElementById("url").value;
   chrome.runtime.sendMessage({
     type: "copy-network-request-response",
-    url: url
+    url: "https://c12.qbo.intuit.com/qbo12/v4/entities"
   });
 });
